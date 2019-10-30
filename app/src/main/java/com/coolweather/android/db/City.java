7b7b0c0bd5cef009b386d54af2602f9243158c88
Tian Ye <978148150@qq.com>
2019-10-30 22:06:09 +0800

    加入创建数据库和表的各项操作。

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,38 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2019-10-30.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getCityName(){
+        return cityName;
+    }
+    public void setCityName(String provinceName){
+        this.cityName=provinceName;
+    }
+    public int getCityCode(){
+        return cityCode;
+    }
+    public void setCityCode(int provinceCode){
+        this.cityCode=provinceCode;
+    }
+    public int getProvinceId(){
+        return provinceId;
+    }
+    public void setProvinceId(int provinceId){
+        this.provinceId=provinceId;
+    }
+}
