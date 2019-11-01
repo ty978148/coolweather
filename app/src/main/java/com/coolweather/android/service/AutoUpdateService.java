666a82c9358674c93ee209f7344ab324ede18b3c
Tian Ye <978148150@qq.com>
2019-11-01 22:48:27 +0800

    修改程序图标和名称

--- app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -29,7 +29,7 @@ public class AutoUpdateService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         updateWeather();
         updateBingPic();
-        AlarmManager manager=(AlarmManager)getSystemService(ACTIVITY_SERVICE);
+        AlarmManager manager=(AlarmManager)getSystemService(ALARM_SERVICE);
         int anHour=8*60*60*1000;
         long triggerAtTime= SystemClock.elapsedRealtime()+anHour;
         Intent i=new Intent(this,AutoUpdateService.class);
